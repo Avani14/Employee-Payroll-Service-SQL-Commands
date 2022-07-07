e4d2144b661c645a7fe36298908bbdc86f23c293
Avani14 <trivediavani14@gmail.com>
2022-07-07 21:37:26 +0530

    SQL command to create the table for payroll service,

--- employee_payroll_service.sql
+++ employee_payroll_service.sql
@@ -2,3 +2,10 @@
 create database employee_payroll_service;
 show databases;
 use employee_payroll_service;
+create table employee_payroll(
+	id int NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	name varchar(50), 
+    salary INT,
+    start_date DATE NOT NULL);
+    
+SELECT * FROM employee_payroll;
